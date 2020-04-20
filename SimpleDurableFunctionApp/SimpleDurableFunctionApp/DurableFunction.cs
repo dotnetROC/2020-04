741f6ef0542e1b70879293e451acae8308bf54ef
Kyle Korndoerfer <kyle.korndoerfer@gmail.com>
2020-04-19 22:16:59 -0400

    Add Queue starter and reorganized

--- SimpleDurableFunctionApp/SimpleDurableFunctionApp/DurableFunction.cs
+++ SimpleDurableFunctionApp/SimpleDurableFunctionApp/DurableFunction.cs
@@ -4,35 +4,16 @@ using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace SimpleDurableFunctionApp
 {
     public static class DurableFunction
     {
-        [FunctionName("Function1")]
-        public static async Task<List<string>> RunOrchestrator(
-            [OrchestrationTrigger] IDurableOrchestrationContext context)
-        {
-            var outputs = new List<string>();
+        #region *** Starter functions ***
 
-            // Replace "hello" with the name of your Durable Activity Function.
-            outputs.Add(await context.CallActivityAsync<string>("Function1_Hello", "Tokyo"));
-            outputs.Add(await context.CallActivityAsync<string>("Function1_Hello", "Seattle"));
-            outputs.Add(await context.CallActivityAsync<string>("Function1_Hello", "London"));
-
-            // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
-            return outputs;
-        }
-
-        [FunctionName("Function1_Hello")]
-        public static string SayHello([ActivityTrigger] string name, ILogger log)
-        {
-            log.LogInformation($"Saying hello to {name}.");
-            return $"Hello {name}!";
-        }
-
+        // *** Starter function with an HTTP Trigger ***
         [FunctionName("Function1_HttpStart")]
         public static async Task<HttpResponseMessage> HttpStart(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestMessage req,
@@ -40,11 +21,66 @@ namespace SimpleDurableFunctionApp
             ILogger log)
         {
             // Function input comes from the request content.
-            string instanceId = await starter.StartNewAsync("Function1", null);
+            string requestBody = await req.Content.ReadAsStringAsync().ConfigureAwait(false);
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            string name = data?.name ?? "Unknown";
+
+            string instanceId = await starter.StartNewAsync<string>("Function1", name);
 
             log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
 
             return starter.CreateCheckStatusResponse(req, instanceId);
+        }
+
+        // *** Starter function with a Queue Trigger ***
+        [FunctionName("Function1_QueueStart")]
+        public static async Task QueueStart(
+            [QueueTrigger("durable-function-trigger", Connection = "QueueConnectionString")] string input,
+            [DurableClient] IDurableOrchestrationClient starter,
+            ILogger log)
+        {
+            // Function input comes from the queue message
+            dynamic data = JsonConvert.DeserializeObject(input);
+            string name = data?.name ?? "Unknown";
+
+            string instanceId = await starter.StartNewAsync<string>("Function1", name);
+
+            log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
+        }
+
+        #endregion
+
+        // *** Orchestrator Function ***
+        [FunctionName("Function1")]
+        public static async Task<List<string>> RunOrchestrator(
+            [OrchestrationTrigger] IDurableOrchestrationContext context,
+            ILogger log)
+        {
+            string name = context.GetInput<string>();
+
+            if (!context.IsReplaying)
+            {
+                log.LogInformation($"Executing Orchestrator with an input of '{name}'");
+            }
+
+            var outputs = new List<string>();
+
+            // Replace "hello" with the name of your Durable Activity Function.
+            outputs.Add(await context.CallActivityAsync<string>("Function1_Hello", $"{name} from Tokyo"));
+            outputs.Add(await context.CallActivityAsync<string>("Function1_Hello", $"{name} from Seattle"));
+            outputs.Add(await context.CallActivityAsync<string>("Function1_Hello", $"{name} from London"));
+
+            // returns ["Hello x from Tokyo!", "Hello x from Seattle!", "Hello x from London!"]
+            log.LogInformation($"Output: { string.Join(", ", outputs ) }");
+            return outputs;
+        }
+
+        // *** Activity Function ***
+        [FunctionName("Function1_Hello")]
+        public static string SayHello([ActivityTrigger] string name, ILogger log)
+        {
+            log.LogInformation($"Saying hello to {name}.");
+            return $"Hello {name}!";
         }
     }
 }
